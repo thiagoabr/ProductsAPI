9d669d5ebe663882742aaee567c9c7cdd212583f
thiagoabr <thiagoabr@gmail.com>
2023-07-26 21:05:54 -0300

    Ajustes no swagger Doc

--- ProductsAPI.Services/Extensions/SwaggerDocExtension.cs
+++ ProductsAPI.Services/Extensions/SwaggerDocExtension.cs
@@ -14,7 +14,7 @@ namespace ProductsAPI.Services.Extensions
                 new OpenApiInfo
                 {
                     Title = "ProductsAPI - COTI Informática",
-                    Description = "API para controle de Produtos - Curso C# Avançado Formação Arquiteto",
+                    Description = "API para controle de produtos - Curso C# Avançado Formação Arquiteto",
                     Version = "v1",
                     Contact = new OpenApiContact
                     {
@@ -24,10 +24,33 @@ namespace ProductsAPI.Services.Extensions
                     }
                 });
 
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
 
-                options.IncludeXmlComments(xmlPath);
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
             });
 
             return services;
--- ProductsAPI.Services/Program.cs
+++ ProductsAPI.Services/Program.cs
@@ -1,30 +1,26 @@
 using MediatR;
 using ProductsAPI.Application.Interfaces.Services;
 using ProductsAPI.Application.Services;
-using ProductsAPI.Services.Extensions;
 using ProductsAPI.Infra.IoC.Extensions;
-
+using ProductsAPI.Services.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerDoc();
-builder.Services.AddDependencyInjection(); //Serviços
+builder.Services.AddSwaggerDoc(); //Swagger
 builder.Services.AddSqlServerConfig(builder.Configuration); //SqlServer
 builder.Services.AddMongoDBConfig(builder.Configuration); //MongoDB
+builder.Services.AddDependencyInjection(); //Serviços
 builder.Services.AddMediatRConfig(); //MediatR
-builder.Services.AddJwtBearer(builder.Configuration); //JWT
+builder.Services.AddJwtBearer(builder.Configuration); //JWT   
+builder.Services.AddCorsPolicy(); //CORS
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerDoc(); //Swagger
+app.UseCorsPolicy(); //CORS
 
-app.UseSwaggerDoc();
 app.UseAuthentication();
 app.UseAuthorization();
 
